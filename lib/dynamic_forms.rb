458e7b870ebe4f5e7b8bfff87bc874ad12d0c2f9
Sam Coles <sam.coles@giantquanta.com>
2016-07-25 19:42:09 -0400

    Adds item_type registry for custom types

--- lib/dynamic_forms.rb
+++ lib/dynamic_forms.rb
@@ -1,5 +1,27 @@
 require "dynamic_forms/engine"
 
 module DynamicForms
-  # Your code goes here...
+
+  ItemTypeDefinition = Struct.new(:name, :type, :description) do
+    def to_class
+      self.type.constantize
+    end
+  end
+
+  mattr_accessor :item_types do
+    [ ]
+  end
+
+  def self.register_item_type(name, type, description)
+    # Don't allow multiple registrations of same type (autoreload)
+    return if self.item_types.map(&:type).include?(type)
+    self.item_types << ItemTypeDefinition.new(name, type, description)
+  end
+
+  # Item types provided by DynamicForms. Yours can be registered
+  # in an initializer by calling DynamicForms.register_item_type(...)
+  register_item_type("Text Question", "DynamicForms::TextQuestion", "A question that collects a text response")
+  register_item_type("Text Block", "DynamicForms::TextBlock", "A title and paragraph content for explainations and introductions")
+
 end
+
